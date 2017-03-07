f00f4e0950dde3721fdc9002f25153d7d6299881
abhinav mandloi <abhinavmandloi@Abhinavs-MacBook-Pro.local>
2017-03-07 16:21:02 +0530

    Initial Commit

---
+++ NewProject/NewProject.h
@@ -0,0 +1,19 @@
+//
+//  NewProject.h
+//  NewProject
+//
+//  Created by Abhinav Mandloi on 07/03/17.
+//  Copyright © 2017 Abhinav Mandloi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NewProject.
+FOUNDATION_EXPORT double NewProjectVersionNumber;
+
+//! Project version string for NewProject.
+FOUNDATION_EXPORT const unsigned char NewProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NewProject/PublicHeader.h>
+
+
